91ffacfa8b4e4918a39113002451d6edad0f92a5
afzytk <afzytk@gmail.com>
2025-12-05 23:06:38 +0530

    feat: add GSAP animation for hero section

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -1,4 +1,34 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+
 const HeroSection = () => {
+  useGSAP(() => {
+    const titleSplit = SplitText.create(".hero-title", { type: "chars" });
+
+    const tl = gsap.timeline({ delay: 1 });
+
+    tl.to(".hero-content", { opacity: 1, y: 0, ease: "power1.inOut" })
+      .to(
+        ".hero-text-scroll",
+        {
+          duration: 1,
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+          ease: "circ.Out",
+        },
+        "-=0.5"
+      )
+      .from(
+        titleSplit.chars,
+        {
+          yPercent: 200,
+          stagger: 0.02,
+          ease: "power2.Out",
+        },
+        "-=0.5"
+      );
+  });
+
   return (
     <section className="bg-main-bg">
       <div className="hero-container">
@@ -8,7 +38,7 @@ const HeroSection = () => {
           className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto scale-100 md:scale-150"
         />
 
-        <div className="hero-content">
+        <div className="hero-content opacity-0">
           <div className="overflow-hidden">
             <h1 className="hero-title">Freaking Delicious</h1>
           </div>
