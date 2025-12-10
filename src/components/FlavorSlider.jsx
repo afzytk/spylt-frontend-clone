1155896b86785b1a4d3bb4b8d2137a9a734b3c88
afzytk <afzytk@gmail.com>
2025-12-11 00:37:03 +0530

    feat: implement FlavorSlider component with flavor cards

--- src/components/FlavorSlider.jsx
+++ src/components/FlavorSlider.jsx
@@ -1,5 +1,38 @@
+import { flavorlists } from "../constants";
+
 const FlavorSlider = () => {
-  return <div>FlavorSlider</div>;
+  return (
+    <div className="slider-wrapper">
+      <div className="flavors">
+        {flavorlists.map((flavor) => (
+          <div
+            key={flavor.name}
+            className={`relative z-30 lg:w-[50vh] w-96 lg:h-[70vh] md:w-[50vh] h-80 flex-none ${flavor.rotation}`}
+          >
+            <img
+              src={`/images/${flavor.color}-bg.svg`}
+              alt=""
+              className="absolute bottom-0 "
+            />
+
+            <img
+              src={`/images/${flavor.color}-drink.webp`}
+              alt={`${flavor.color} drink`}
+              className="drinks"
+            />
+
+            <img
+              src={`/images/${flavor.color}-elements.webp`}
+              alt=""
+              className="elements"
+            />
+
+            <h1>{flavor.name}</h1>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default FlavorSlider;
