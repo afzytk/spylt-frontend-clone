5a4e985928e63df13ced9671f28610f3b875f0b0
afzytk <afzytk@gmail.com>
2025-12-18 17:38:33 +0530

    feat: disable horizontal GSAP scroll animation on tablets

--- src/components/FlavorSlider.jsx
+++ src/components/FlavorSlider.jsx
@@ -2,27 +2,34 @@ import { useGSAP } from "@gsap/react";
 import { flavorlists } from "../constants";
 import gsap from "gsap";
 import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const FlavorSlider = () => {
   const sliderRef = useRef();
 
+  const isTablet = useMediaQuery({
+    query: "(max-width 1024)",
+  });
+
   useGSAP(() => {
     const scrollAmount = sliderRef.current.scrollWidth - window.innerWidth;
 
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".flavor-section",
-        start: "2% top",
-        end: `+=${scrollAmount + 1500}px`,
-        scrub: true,
-        pin: true,
-      },
-    });
+    if (!isTablet) {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".flavor-section",
+          start: "2% top",
+          end: `+=${scrollAmount + 1500}px`,
+          scrub: true,
+          pin: true,
+        },
+      });
 
-    tl.to(".flavor-section", {
-      x: `-${scrollAmount + 1500}px`,
-      ease: "power1.inOut",
-    });
+      tl.to(".flavor-section", {
+        x: `-${scrollAmount + 1500}px`,
+        ease: "power1.inOut",
+      });
+    }
 
     const titleTl = gsap.timeline({
       scrollTrigger: {
